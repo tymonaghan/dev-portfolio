9b58c7b5e7effef656a43ccd6c3fc43e0344c7a0
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-02-24 22:30:31 -0600

    hello world: react, webpack and webpack-server, tailwind

---
+++ app/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TestCard from "./TestCard";
+
+ReactDOM.render(
+  <div>
+    <p>It's working, hooray!</p>
+    <TestCard />
+  </div>,
+  document.getElementById("app")
+);
