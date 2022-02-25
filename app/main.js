6d30acb423294a33af5a45d348386b6112536fc8
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-02-25 14:24:32 -0600

    integrate tailwind css with a demo card
    time to dig in!

--- app/main.js
+++ app/main.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import TestCard from "./TestCard";
 
 ReactDOM.render(
-  <div>
-    <p>It's working, hooray!</p>
+  <div className="w-96 m-20 p-2 bg-green-500 shadow rounded">
+    <p>Hi there</p>
     <TestCard />
   </div>,
   document.getElementById("app")
