e096c4b2f21eb58bd50c8c0bd0841052b834ca49
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-02-26 16:51:36 -0600

    dark mode support
    thanks, tailwind CSS!

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="w-full p-6 text-center">
-      <div className="flex-col">
+    <div className="w-full p-6 text-center text-black dark:text-white">
+      <div className="flex-col ">
         <div>
-          <p className="text-md sm:text-lg text-left">
+          <p className="text-md sm:text-lg text-left ">
             Hi, I'm Tyler, a software engineer and digital humanitarian. Welcome
             to my digital development portfolio!{" "}
           </p>
--- app/components/ProjectList.js
+++ app/components/ProjectList.js
@@ -3,7 +3,7 @@ import projectData from "../projectData";
 
 const ProjectList = () => {
   return (
-    <div>
+    <div className="text-black dark:text-white">
       This page is a work in progress!
       {projectData.projects.map((project, key) => {
         return (
@@ -11,12 +11,10 @@ const ProjectList = () => {
             key={key}
             className="flex max-w-xl  max-h-40 m-6 p-4 mx-auto bg-slate-600 hover:bg-sky-700 border-slate-300 border-2  shadow rounded-md space-x-4"
           >
-            <div className="text-white font-bold">{project.name}</div>
-            <div className="text-white">{project.repo}</div>
-            <div className="text-white">{project.site}</div>
-          </div>
-        );
-      })}
+              <div className="font-bold">{project.name}</div>
+              <div className="">{project.repo}</div>
+              <div className="">{project.site}</div>
+            </div>
     </div>
   );
 };
